c5d0ab91fac5a4acbe1845d3743b5ea0f896983d
zhangzhenli <zhangzhenli@live.com>
2016-04-11 12:39:40 +0800

    2.0.0

--- builder-model/src/com/android/builder/model/Version.java
+++ builder-model/src/com/android/builder/model/Version.java
@@ -3,6 +3,6 @@ package com.android.builder.model;
 
 public final class Version {
     private Version() {}
-    public static final String ANDROID_GRADLE_PLUGIN_VERSION = "2.0.0-rc3";
+    public static final String ANDROID_GRADLE_PLUGIN_VERSION = "2.0.0";
     public static final int BUILDER_MODEL_API_VERSION = 3;
 }
